61b407fb9051289f0596f8b021fafc59423514c0
Yuky オ <90131020+angelofran@users.noreply.github.com>
2023-08-09 16:03:43 +0100

    Update

--- src/main.py
+++ src/main.py
@@ -1,21 +1,20 @@
-valores = list(map(float, input().split()))
-A, B, C = sorted(valores, reverse = True)
+A, B, C = sorted(map(float, input().split()), reverse = True)
 
 if A >= B + C:
     print('NAO FORMA TRIANGULO')
 
 else:
-    if (A ** 2) == (B ** 2) + (C ** 2):
+    if A ** 2 == B ** 2 + C ** 2:
         print('TRIANGULO RETANGULO')
 
-    if (A ** 2) > (B ** 2) + (C ** 2):
+    if A ** 2 > B ** 2 + C ** 2:
         print('TRIANGULO OBTUSANGULO')
 
-    if (A ** 2) < (B ** 2) + (C ** 2):
+    if A ** 2 < B ** 2 + C ** 2:
         print('TRIANGULO ACUTANGULO')
 
     if  A == B == C:
         print('TRIANGULO EQUILATERO')
 
-    if A == B or A == C or B == C:
+    elif A == B or B == C or A == C:
         print("TRIANGULO ISOSCELES")
