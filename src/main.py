17aa8ec977a43f43ec044afb575b96bfae7e2526
Yuky オ <90131020+angelofran@users.noreply.github.com>
2023-08-09 15:51:13 +0100

    Updated

--- src/main.py
+++ src/main.py
@@ -4,17 +4,18 @@ A, B, C = sorted(valores, reverse = True)
 if A >= B + C:
     print('NAO FORMA TRINAGULO')
 
-if (A ** 2) == (B ** 2) + (C ** 2):
-    print('TRIANGULO RETANGULO')
+else:
+    if (A ** 2) == (B ** 2) + (C ** 2):
+        print('TRIANGULO RETANGULO')
 
-if (A ** 2) > (B ** 2) + (C ** 2):
-    print('TRIANGULO OBTUSANGULO')
+    if (A ** 2) > (B ** 2) + (C ** 2):
+        print('TRIANGULO OBTUSANGULO')
 
-if (A ** 2) < (B ** 2) + (C ** 2):
-    print('TRIANGULO ACUTANGULO')
+    if (A ** 2) < (B ** 2) + (C ** 2):
+        print('TRIANGULO ACUTANGULO')
 
-if  A == B == C:
-    print('TRIANGULO EQUILATERO')
+    if  A == B == C:
+        print('TRIANGULO EQUILATERO')
 
-if A == B or A == C or B == C:
-    print("TRIANGULO ISOSCELES")
+    if A == B or A == C or B == C:
+        print("TRIANGULO ISOSCELES")
